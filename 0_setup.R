136b610402f1270c2d20025fcd65286d605ade5a
magali17 <magalipdx@gmail.com>
2024-06-12 15:59:22 -0700

    ds

--- 0_setup.R
+++ 0_setup.R
@@ -12,21 +12,18 @@ if (!is.null(sessionInfo()$otherPkgs)) {
 ####################################################################
 # install ufp_model package 
 ## you may need to use the auth_token parameter to add a persona access token (PAT) in order to install something from a private repo. The PAT needs to have at least repo scope: https://github.com/settings/tokens 
+## ?? if using the cluster, do this on the head node?
 
 # remotes::install_github('karr-lab/ufp_model', auth_token = "ADD YOUR TOKEN HERE")
 
 ####################################################################
-pacman::p_load(ufp.model.saha2021,
-               tidyverse)
+# pacman::p_load(ufp.model.saha2021,
+#                tidyverse)
 
 output_path <- file.path("output", "predictions", "original_model", "blocks", "raw")
 
 # create directories
 lapply(c(output_path,
-         #file.path(output_path, "txt"),
-         #file.path(output_path, "rda"),
-         #file.path("output", "predictions", "original_model", "blocks","modified",  "txt"),
-         #file.path("output", "predictions", "original_model", "blocks", "modified", "rda"),
         file.path("output", "predictions", "original_model", "blocks", "modified"),
          file.path("output", "qc"),
          file.path("output", "winsorize")
