c404ac2b2418a8de3c6ee8c33c69abf46a9d0656
magali17 <magalipdx@gmail.com>
2024-06-12 15:50:49 -0700

    other files

---
+++ 1a_predict_with_og_model.R
@@ -0,0 +1,68 @@
+# scripts imports 2010 & 2020 block covariates and generates predictions with the original model
+# output is here: output/predictions/original_model/blocks/raw 
+
+####################################################################
+# SETUP
+####################################################################
+# Clear workspace of all objects and unload all extra (non-base) packages
+rm(list = ls(all = TRUE))
+if (!is.null(sessionInfo()$otherPkgs)) {
+  res <- suppressWarnings(
+    lapply(paste('package:', names(sessionInfo()$otherPkgs), sep=""),
+           detach, character.only=TRUE, unload=TRUE, force=TRUE))}
+
+pacman::p_load(ufp.model.saha2021, tidyverse)
+
+# if prediction files already exist, should these be overwritten?
+override_predictions <- FALSE
+
+covariate_path <- file.path("input")
+predictions_path <- file.path("output", "predictions", "original_model", "blocks", "raw")
+
+####################################################################
+# BLOCK PREDICTIONS
+####################################################################
+covariate_files <- list.files(covariate_path)
+
+# x= covariate_files[2]
+lapply(covariate_files, function(x) {
+  new_prediction_file <- file.path(predictions_path, x)
+  
+  if(!file.exists(new_prediction_file) | 
+     override_predictions==TRUE){
+    message(paste("generating new predictions for", x))
+    
+    # read in covariate file
+    covariates <- readRDS(file.path(covariate_path, x)) %>%
+      rename(block_key = native_id,
+             latitude = lat_block, 
+             longitude= long_block,
+             
+             # ll_a1_a3_s03000 = ll_a1_s03000 + ll_a3_s03000 
+             ## ll_a1_a3_s03000 already exists in these particular datasets. manipulate this so the function doesn't error out
+             ll_a1_s03000=ll_a1_a3_s03000) %>% 
+      mutate(ll_a3_s03000 = 0)
+      
+      
+    # generate UFP predictions. 
+    ## function will generate additional covariates and makes modifications as necessary
+    predictions <- predictUFP(covariates)
+    
+    # save predictions
+    saveRDS(predictions, new_prediction_file)
+  } else {
+    message(paste("predictions for", x, "already exist"))
+    }
+  }) 
+
+
+####################################################################
+#  
+####################################################################
+
+
+
+
+
+
+
---
+++ 1b_winsorize_predictions.R
@@ -0,0 +1,117 @@
+# Script winsorizes extreme UFP predictions using 2010 & 2020 covariates
+# output is here: output/predictions/original_model/blocks/modified 
+
+####################################################################
+# SETUP
+####################################################################
+# Clear workspace of all objects and unload all extra (non-base) packages
+rm(list = ls(all = TRUE))
+if (!is.null(sessionInfo()$otherPkgs)) {
+  res <- suppressWarnings(
+    lapply(paste('package:', names(sessionInfo()$otherPkgs), sep=""),
+           detach, character.only=TRUE, unload=TRUE, force=TRUE))}
+
+pacman::p_load(tidyverse, parallel)
+
+input_path <- file.path("output", "predictions", "original_model", "blocks", "raw")
+output_path <- file.path("output", "predictions", "original_model", "blocks", "modified")
+
+prediction_files <- list.files(input_path)
+quantile_files <- paste0("raw_quantiles_", prediction_files)
+
+use_cores <- 4 #6 is slow during winsorizing???
+
+####################################################################
+
+testing_mode <- TRUE
+override_quantile_file <- TRUE # TRUE if e.g., updating missing block covariates
+override_winsorized_file <- TRUE
+
+####################################################################
+# COMMON VARIABLES
+####################################################################
+
+# winsorizing thresholds
+low_quantile <- 0.02
+## results in max conc ~25k pt/cm3. Saha paper & block prediction max is ~26,579. anything higher produces much higher conc's (99th quantile ~105k)
+high_quantile <- 0.98
+#winsorizing_label <- paste0(low_quantile, high_quantile, sep="_")
+####################################################################
+# CALCULATE DATASET QUANTILES FOR WINSORIZING
+####################################################################
+# f=quantile_files[2]
+lapply(c(quantile_files), function(f){
+  
+  this_quantile_file <- file.path("output", "winsorize", f)
+  
+  if(!file.exists(this_quantile_file) | 
+     override_quantile_file == TRUE) {
+    
+    message("reading in raw UFP prediction file")
+    predictions <- readRDS(file.path(input_path, gsub("raw_quantiles_", "", f))) 
+    quantile_list <-seq(0, 1, 0.01)
+    
+    if(testing_mode==TRUE){
+      message("...testing mode")
+      predictions <- predictions[1:1e3,]
+      quantile_list <- quantile_list[1:2]
+      }
+    
+    message(paste("calculating UFP quantiles:", this_quantile_file))
+    ufp_quantiles <- mclapply(quantile_list, mc.cores = use_cores, function(q){
+      tibble(quantile = q,
+             
+             # --> TEMP: some blocks have missing values for 'imp_a00750' 
+             conc = quantile(predictions$ufp, q, na.rm = T))
+    }) %>%
+      bind_rows()
+    
+    message("...saving UFP quantile file")
+    saveRDS(ufp_quantiles, this_quantile_file)
+  } else{
+    message(paste("reading in existing UFP quantiles:", this_quantile_file))
+    ufp_quantiles <- readRDS(this_quantile_file)
+  }
+})
+
+####################################################################
+# WINSORIZE PREDICTIONS
+####################################################################
+# 11/28/24 email (Re: Missing values in the national UFP model): based on my analyses in the qc directory, ~2% of blocks (with pop >=1) were dropped because of extreme covariate values (outside the covariate space)
+# blocks that were dropped have a slightly different distribution, with the largest discrepancy being that blocks dropped on average had higher predictions, but not always
+# we want predictions for these locations b/c we have ECHO participants living in these areas 
+
+# f=prediction_files[1]
+mclapply(prediction_files, mc.cores = use_cores, function(f) {
+  
+  raw_file <- file.path(input_path, f)
+  modified_file <- file.path(output_path, f)
+  
+  if(!file.exists(modified_file) | 
+     override_winsorized_file == TRUE) {
+    message("winsorizing values")
+    
+  # winsorization values for specific year covariates
+  ufp_quantiles <- readRDS("output", "winsorize", paste0("raw_quantiles_", f))
+  low_conc <- ufp_quantiles$conc[ufp_quantiles$quantile==low_quantile] %>% as.numeric()
+  high_conc <- ufp_quantiles$conc[ufp_quantiles$quantile==high_quantile] %>% as.numeric()
+  
+  raw_predictions <- readRDS(raw_file)  
+  
+  if(testing_mode==TRUE){
+    message("...testing mode")
+    raw_predictions <- raw_predictions[1:1e3,]
+    }
+    
+  predictions_modified <- raw_predictions %>% 
+    mutate(ufp_winsorized = ifelse(ufp < low_conc, low_conc, 
+                                   ifelse(ufp > high_conc, high_conc, ufp)),
+           winsorized = ifelse(ufp < low_conc | ufp > high_conc, TRUE, FALSE))
+  
+  # save modified predictions
+  saveRDS(predictions_modified, modified_file)
+  }
+  
+})
+
+ 
