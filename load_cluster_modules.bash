36a288c8a842c7ab5487194c082a2aee1ae37d5a
magali17 <magalipdx@gmail.com>
2024-06-13 09:48:08 -0700

    d

---
+++ load_cluster_modules.bash
@@ -0,0 +1,7 @@
+# modules needed to run most scripts in the Brain Cluster
+
+module load GCC/gcc-10.3.0 
+module load GEOS/geos-3.8.0 
+module load GDAL/gdal-3.0.2 
+module load UDUNITS/udunits-2.2.26 
+module load R/R-4.2.2
